f7c157b29e1bcf244b56e92b28da50ebcc110d48
DaniyarMashayev <dmashaev@gmail.com>
2023-11-22 15:56:56 +0600

    update GreatestComminDivisor.java

--- app/src/main/java/hexlet/code/games/GreatestCommonDivisor.java
+++ app/src/main/java/hexlet/code/games/GreatestCommonDivisor.java
@@ -1,9 +1,12 @@
 package hexlet.code.games;
 public class GreatestCommonDivisor {
-    public static String checkAnswer(int number1, int number2) {
+    public static String checkAnswerGCD(int number1, int number2) {
         if (number2 == 0) {
             return String.valueOf(number1);
         }
-        return checkAnswer(number2, number1 % number2);
+        return checkAnswerGCD(number2, number1 % number2);
+    }
+    public static String printQuestionGCD(int number1, int number2) {
+        return String.valueOf(number1) + " " + String.valueOf(number2);
     }
 }
