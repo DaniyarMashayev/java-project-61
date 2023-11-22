8b697fd49f15e8db80e9477752a4b7fbacd10a19
DaniyarMashayev <dmashaev@gmail.com>
2023-11-22 15:56:02 +0600

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,7 +1,9 @@
 package hexlet.code.games;
 
+import java.util.Random;
+
 public class Calc {
-    public static String checkAnswer(int number1, int number2, String operator) {
+    public static String checkAnswerCalc(int number1, int number2, String operator) {
         int resultCalc = 0;
         if (operator.equals("+")) {
             resultCalc = number1 + number2;
@@ -11,5 +13,22 @@ public class Calc {
             resultCalc = number1 * number2;
         }
         return String.valueOf(resultCalc);
+    }
+    public static String printQuestionCalc(int number1, int number2, String operator) {
+//        String operator = generatorMathOperator();
+        return String.valueOf(number1) + " " + operator + " " + String.valueOf(number2);
+    }
+    public static String generatorMathOperator() {
+        Random rand = new Random();
+        int number = rand.nextInt(3);
+        String operator = " ";
+        if (number == 0) {
+            operator = "+";
+        } else if (number == 1) {
+            operator = "-";
+        } else if (number == 2) {
+            operator = "*";
+        }
+        return operator;
     }
 }
