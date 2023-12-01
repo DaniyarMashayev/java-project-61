dd0dfbd8be6b78895da01eade5dd1a4884a613d3
DaniyarMashayev <dmashaev@gmail.com>
2023-12-02 01:54:06 +0600

    change getCorrectAnswer method to findGCD

--- app/src/main/java/hexlet/code/games/GreatestCommonDivisor.java
+++ app/src/main/java/hexlet/code/games/GreatestCommonDivisor.java
@@ -1,31 +1,34 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
+import static hexlet.code.Engine.COUNTOFROUND;
 
 public class GreatestCommonDivisor {
-    private static final int COUNTOFROUND = 3;
+    private static final String GAMERULES = "Find the greatest common divisor of given numbers.";
 
     public static void runGCD() {
-        String rules = "Find the greatest common divisor of given numbers.";
         String[][] questionsAndCorrectAnswers = getQuestionsAndCorrectAnswers();
-        Engine.runEngine(rules, questionsAndCorrectAnswers);
+        Engine.runEngine(GAMERULES, questionsAndCorrectAnswers);
     }
 
     private static String[][] getQuestionsAndCorrectAnswers() {
         String[][] questionsAndCorrectAnswers = new String[COUNTOFROUND][2];
         for (int i = 0; i < COUNTOFROUND; i++) {
-            int number1 = Engine.generatorNumber();
-            int number2 = Engine.generatorNumber();
-            questionsAndCorrectAnswers[i][0] = (number1) + " " + (number2);
-            questionsAndCorrectAnswers[i][1] = getCorrectAnswer(number1, number2);
+            int number1 = Utils.generatorNumber();
+            int number2 = Utils.generatorNumber();
+            String answer = (number1) + " " + (number2);
+            String correctAnswer = String.valueOf(findGCD(number1, number2));
+            questionsAndCorrectAnswers[i][0] = answer;
+            questionsAndCorrectAnswers[i][1] = correctAnswer;
         }
         return questionsAndCorrectAnswers;
     }
 
-    private static String getCorrectAnswer(int number1, int number2) {
+    private static int findGCD(int number1, int number2) {
         if (number2 == 0) {
-            return String.valueOf(number1);
+            return number1;
         }
-        return getCorrectAnswer(number2, number1 % number2);
+        return findGCD(number2, number1 % number2);
     }
 }
