1cef113e25511725c95b2ef4c80d665c1e96846e
DaniyarMashayev <dmashaev@gmail.com>
2023-12-02 01:52:21 +0600

    change getCorrectAnswer method to isEven method

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,27 +1,30 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
+import static hexlet.code.Engine.COUNTOFROUND;
 
 public class Even {
-    private static final int COUNTOFROUND = 3;
+    private static final String GAMERULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
     public static void runEven() {
-        String rules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         String[][] questionsAndCorrectAnswers = getQuestionsAndCorrectAnswers();
-        Engine.runEngine(rules, questionsAndCorrectAnswers);
+        Engine.runEngine(GAMERULES, questionsAndCorrectAnswers);
     }
 
     private static String[][] getQuestionsAndCorrectAnswers() {
         String[][] questionsAndCorrectAnswers = new String[COUNTOFROUND][2];
         for (int i = 0; i < COUNTOFROUND; i++) {
-            int number = Engine.generatorNumber();
-            questionsAndCorrectAnswers[i][0] = String.valueOf(number);
-            questionsAndCorrectAnswers[i][1] = getCorrectAnswer(number);
+            int number = Utils.generatorNumber();
+            String question = String.valueOf(number);
+            String correctAnswer = isEven(number) ? "yes" : "no";
+            questionsAndCorrectAnswers[i][0] = question;
+            questionsAndCorrectAnswers[i][1] = correctAnswer;
         }
         return questionsAndCorrectAnswers;
     }
 
-    private static String getCorrectAnswer(int number) {
-        return number % 2 == 0 ? "yes" : "no";
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
